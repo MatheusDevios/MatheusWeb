b88231072062f7f621a70a5343e6fdfbd2e3bb98
Matheus Diniz <matheus.diniiiiz@gmail.com>
2022-06-22 23:59:52 +0100

    scroll function changed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -10,20 +10,7 @@ import { HiSpeakerphone } from 'react-icons/hi'
 // import { useState } from 'react'
 
 const Nav = () => {
-  // const [activeNav, setActiveNav] = useState('#home')
   const sections = document.querySelectorAll('section[id]')
-
-  // function activeLink() {
-  //     sections.forEach((item)=>{
-  //         item.classList.remove('active-link');
-  //         this.classList.add('active-link')
-  //     })
-  // }
-  // sections.forEach((item) =>{
-  //     item.addEventListener('click', activeLink)
-  // })
-  
-  console.log(sections);
 
   function scrollActive() {
     const scrollY = window.pageYOffset
@@ -41,6 +28,8 @@ const Nav = () => {
     })
   }
   window.addEventListener('scroll', scrollActive)
+
+
   return (
     <nav className='nav__menu'>
       <a href="#home" ><AiTwotoneHome /></a>
@@ -51,15 +40,6 @@ const Nav = () => {
       <a href="#testimonials" ><HiSpeakerphone /></a>
       <a href="#contact" ><RiMessage2Fill /></a>
     </nav>
-    // <nav className='nav__menu'>
-    //   <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#home' ? 'active' : ''}><AiTwotoneHome /></a>
-    //   <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><FaUserSecret /></a>
-    //   <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><GiBookCover /></a>
-    //   <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceFill /></a>
-    //   <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiReactjsLine /></a>
-    //   {/* <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><HiSpeakerphone /></a> */}
-    //   <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><RiMessage2Fill /></a>
-    // </nav>
   )
 }
 
