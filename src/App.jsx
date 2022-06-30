a7481390c1216f5d2bac55e90ff99d0f84f2c1e9
Matheus Diniz <matheus.diniiiiz@gmail.com>
2022-06-30 23:18:51 +0100

    useLayoutEffect added to loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useLayoutEffect } from 'react'
 import Header from './components/Header'
 import Nav from './components/Nav'
 import About from './components/About'
@@ -16,7 +16,7 @@ function App() {
 
     const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
+    useLayoutEffect(() => {
         setLoading(true);
         setTimeout(() => {
             setLoading(false);
