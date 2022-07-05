1508c82dfa58d733ca5fd227b21ba08983324f68
Matheus Diniz <matheus.diniiiiz@gmail.com>
2022-07-05 13:49:25 +0100

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import './header.css'
-import CallToAction from './CallToAction'
-import HeaderSocials from './HeaderSocials'
-import TypeAnimation from 'react-type-animation';
+import React from "react";
+import "./header.css";
+import CallToAction from "./CallToAction";
+import HeaderSocials from "./HeaderSocials";
+import TypeAnimation from "react-type-animation";
 
 const Header = () => {
   return (
@@ -11,9 +11,9 @@ const Header = () => {
         <h5>Hi there, my name is</h5>
         <h1>Matheus Diniz</h1>
         <TypeAnimation
-          className='type-animation'
+          className="type-animation"
           cursor={true}
-          sequence={[' Full Stack Developer.', 1500, ' iOS Developer.', 1500]}
+          sequence={[" Web Developer.", 1500, " iOS Developer.", 1500]}
           wrapper="h5"
           repeat={Infinity}
         />
@@ -21,13 +21,20 @@ const Header = () => {
         <HeaderSocials />
 
         <div className="me">
-          <img src={'https://res.cloudinary.com/dotuxkpjj/image/upload/v1656600720/me_hioxee.png'} alt="me" />
+          <img
+            src={
+              "https://res.cloudinary.com/dotuxkpjj/image/upload/v1656600720/me_hioxee.png"
+            }
+            alt="me"
+          />
         </div>
 
-        <a href="#contact" className='scrollDown'>Let's Talk</a>
+        <a href="#contact" className="scrollDown">
+          Let's Talk
+        </a>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
